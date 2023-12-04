36527186ec18f1ba1d00762558e04c51df823bd2
K-T-P <koroush.t81@gmail.com>
2023-12-05 00:35:38 +0330

    ImportData function added to add data from user (terminal).

--- CFG.cs
+++ CFG.cs
@@ -5,5 +5,21 @@ namespace CFG{
         static void Main(){
 
         }
+        static void ImportData()
+        {
+            int numberOfLoops = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                string[] data = Console.ReadLine().Split(" -> ");
+                Products.Add(data[0].Trim(), data[1].Split(" | ").Select(x => x.Trim()).ToList());
+
+                //first variable is assigned as start variable
+                if (i == 0)
+                    startVariable = data[0].Trim();
+            }
+            input = Console.ReadLine();
+        }
+        static Dictionary<string, List<string>> Products = new Dictionary<string, List<string>>();
+        static string input;
     }
 }
